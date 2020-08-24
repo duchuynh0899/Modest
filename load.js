44b1e56ae5171409b588b3fca73f09446eac1000
duchuynh0899 <54980175+duchuynh0899@users.noreply.github.com>
2020-08-24 20:21:40 +0700

    up

--- load.js
+++ load.js
@@ -1,24 +1,23 @@
-function startjs() {
-
-    $(document).ready(function() {
-
-        setTimeout(function() {
-            $('body').addClass('loaded');
-            $('h1').css('color', '#222222');
-        }, 1000);
-
+// create config object: rootMargin and threshold
+// are two properties exposed by the interface
+const config = {
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 0
+  };
+  
+  // register the config object with an instance
+  // of intersectionObserver
+  let observer = new intersectionObserver(function(entries, self) {
+    // iterate over each entry
+    entries.forEach(entry => {
+      // process just the images that are intersecting.
+      // isIntersecting is a property exposed by the interface
+      if(entry.isIntersecting) {
+        // custom function that copies the path to the img
+        // from data-src to src
+        preloadImage(entry.target);
+        // the image is now in place, stop watching
+        self.unobserve(entry.target);
+      }
     });
-
-    window.onload = function() {
-        var headerall = document.getElementById('all');
-
-        headerall.style.opacity = 1;
-    };
-
-
-}
-
-window.addEventListener('DOMContentLoaded', function(event) {
-    console.log('html da tai xong');
-    startjs();
-})
+  }, config);
